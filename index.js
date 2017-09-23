8778bc329fde8560fca4c8cf5ad9bb43f55afe69
admin <admin@admins-MBP.fios-router.home>
2017-09-23 12:19:12 -0400

    Error fixing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
-const nock = require('nock'); // for testing
+//const nock = require('nock'); // for testing
 let global;
 
 var flightMap = new Map();
