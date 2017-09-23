584318a6b484d0c8dbc4e511ac8726fa801c19c3
admin <admin@admins-MBP.fios-router.home>
2017-09-23 16:59:49 -0400

    Added post put and delete

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var app = express();
 var fetch = require('node-fetch');
 //var nock = require('nock'); // for testing
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 var global;
-
 var flightMap = new Map();
 
+
+// Class to create object
 class Flight {
-    constructor(id, year, date, name, type, location, pType, pMass, detail){
+    constructor(id, year, date, name, type, location, pType, pMass, detail, aLink, vLink){
         this.id = id;
         this.year = year;
         this.date = date;
@@ -17,9 +21,12 @@ class Flight {
         this.pType = pType;
         this.pMass = pMass;
         this.detail = detail;
+        this.aLink = aLink;
+        this.vLink = vLink;
     }
 }
 
+// Fetch
 fetch('https://api.spacexdata.com/v1/launches')
     .then((res) => {
     return res.json();  
@@ -27,9 +34,10 @@ fetch('https://api.spacexdata.com/v1/launches')
     global = json;
 });
 
-setTimeout(function(){ 
+// Set timeout
+setTimeout(function(){
     global.forEach(function(item){
-        let flight = new Flight(item.flight_number, item.launch_year, item.launch_date_utc, item.rocket.rocket, item.rocket.rocket_type, item.launch_site.site_name, item.payloads.payload_type, item.payloads.payload_mass_kg, item.details);
+        let flight = new Flight(item.flight_number, item.launch_year, item.launch_date_utc, item.rocket.rocket, item.rocket.rocket_type, item.launch_site.site_name, item.payloads[0].payload_type, item.payloads[0].payload_mass_kg, item.details, item.links.article_link, item.links.video_link);
         flightMap.set(item.flight_number, flight);
     });
 }, 2000);
@@ -48,13 +56,40 @@ app.get('/', function(req, res) {
 
 app.get('/launches', function(req, res) {
   var myArray = [];
-  for(var i = 0; i < flightMap.size; i++){
+  for(var i = 0; i <= flightMap.size; i++){
       myArray.push(flightMap.get(i));
   }
     
-  res.send(JSON.stringify(myArray, null, 4));
+  res.send(myArray);
+  //res.send(global);
 });
 
+//Update
+app.put('/update/:id', function(req, res){
+   let id = parseInt(req.params.id);
+   let flight = new Flight(id, req.body.year, req.body.date, req.body.name, req.body.type, req.body.location, req.body.pType, req.body.pMass, req.body.aLink, req.body.vLink);
+    
+   flightMap.set(id, flight);
+   res.redirect('/launches');
+});
+
+// Post to create
+app.post('/add', function(req, res){
+    let id = flightMap.size + 1; 
+    let flight = new Flight(id, req.body.year, req.body.date, req.body.name, req.body.type, req.body.location, req.body.pType, req.body.pMass, req.body.aLink, req.body.vLink);
+    
+    flightMap.set(id, flight);
+    res.redirect('/launches');
+});
+
+// Delete to delete
+app.delete('/remove/:id', function(req, res){
+    let id = parseInt(req.params.id);
+    flightMap.delete(id);
+    res.redirect('/launches');
+});
+
+// By id
 app.get('/launches/:id', function(req, res) {
   let id = parseInt(req.params.id);
   res.send(flightMap.get(id));
